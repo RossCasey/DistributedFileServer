a8ddef7f16e6f8175e61432391b25b5319864fd0
Ross Casey <caseyr4@tcd.ie>
2015-10-14 10:46:31 +0100

    Added thread pool functionality

--- src/EchoServer.scala
+++ src/EchoServer.scala
@@ -6,17 +6,35 @@
 import scala.io._
 import java.io._
 import java.net._
+import java.util.concurrent.Executors
 
 object EchoServer {
+
+  var serverSocket: ServerSocket = null
+
   def main(args: Array[String]) {
-    val serverSocket = new ServerSocket(8000)
+    serverSocket = new ServerSocket(8000)
+    val threadPool = Executors.newFixedThreadPool(2)
 
     println("Listening on port 8000:")
-    while(true) {
-      val s = serverSocket.accept()
-      println("connection accepted")
-      val serverThread = new ServerThread(s)
-      serverThread.start()
+
+    var exit = false
+    while(!exit) {
+      try {
+        val s = serverSocket.accept()
+        println("Connection accepted")
+        val serverThread = new ServerThread(s, killServer)
+        threadPool.execute(serverThread)
+      } catch {
+        case Exception => {
+          exit = true
+          println("Killing server...")
+        }
+      }
     }
+  }
+
+  def killServer(): Unit = {
+    serverSocket.close()
   }
 }
