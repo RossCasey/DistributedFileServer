0cd8cd5cb8022f74c3c2c6ef565feaef70faf923
Ross Casey <caseyr4@tcd.ie>
2015-12-23 14:53:46 +0000

    Sync between nodes now working

--- Code/src/EchoClient.scala
+++ Code/src/EchoClient.scala
@@ -19,8 +19,9 @@ object EchoClient {
     //test.printContents()
     //test.close()
 
+    /*
     val connection = new Connection(0, new Socket(InetAddress.getByName("localhost"), 8000))
-    val dict = ServerMessageHandler.getNode(connection, "hello.txt", false)
+    val dict = ServerMessageHandler.getNode(connection, "rattle.mp3", false)
 
     if(!dict.contains("error")) {
       val nodeAddress = dict("address").asInstanceOf[NodeAddress]
@@ -31,7 +32,7 @@ object EchoClient {
       val priCon = new Connection(1, new Socket(InetAddress.getByName(ipAddress), Integer.parseInt(port)))
 
 
-      val byteArray = Files.readAllBytes(Paths.get("./start.sh"))
+      val byteArray = Files.readAllBytes(Paths.get("./rattle.mp3"))
       ServerMessageHandler.sendUploadRequest(priCon, fileId, byteArray.length)
       priCon.sendBytes(byteArray)
 
@@ -41,7 +42,35 @@ object EchoClient {
     } else {
       println(dict("error"))
     }
+  */
 
+    val connection = new Connection(0, new Socket(InetAddress.getByName("localhost"), 8000))
+    val dict = ServerMessageHandler.getNode(connection, "rattle.mp3", true)
+
+    val node = dict("address").asInstanceOf[NodeAddress]
+    val id= dict("id").asInstanceOf[String]
+
+
+    lazy val socket = new Socket(InetAddress.getByName(node.getIP), Integer.parseInt(node.getPort))
+    val readCon = new Connection(0, socket)
+
+    var count = ServerMessageHandler.requestFile(readCon, id)
+    val baos = new ByteArrayOutputStream()
+
+    while (count != 0) {
+      baos.write(readCon.readByte())
+      count -= 1
+    }
+
+    val fos = new FileOutputStream("./Files/rattle.mp3")
+    val bos = new BufferedOutputStream(fos)
+
+    bos.write(baos.toByteArray)
+    bos.flush()
+    bos.close()
+    fos.close()
+
+    socket.close()
   }
 
 
